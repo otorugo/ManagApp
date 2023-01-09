17724cc02ff07e68dee96e524e8b9a61886f755a
Victor <victorsilva698@gmail.com>
2023-01-08 22:17:48 -0300

    editing intro activity

--- app/src/main/java/com/example/manag/SplashActivity.kt
+++ app/src/main/java/com/example/manag/SplashActivity.kt
@@ -2,10 +2,21 @@ package com.example.manag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        )
+
+
     }
 }
